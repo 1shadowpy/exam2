e59b2624f7863664696224c78668e355ee1d89d4
1shadowpy <132641739+1shadowpy@users.noreply.github.com>
2023-06-02 11:31:07 +0500

    first commit

---
+++ exam.py
@@ -0,0 +1,84 @@
+# oilam = {'otam':'xojiakbar','onam':'dildora','ukam':'honazimbek'}
+# print(oilam.keys())
+# print(oilam.values())
+
+# oilam = {'otam':'xojiakbar','onam':'dildora','ukam':'honazimbek'}
+# for k,q in oilam.items():
+#     print(f"{k} {q}")
+#
+# print("nesting bu ro'yxat ichida lug'at, lug'at ichida ro'yxat")
+# onions = {'onin':'piyoz'}
+# onion = [onions]
+# print(onion)
+
+# print("Kiritilgan sonning kvadratini qaytaruvchi dastur.")
+# savol = "Istalgan son kiriting "
+# savol += "(dasturni to'xtatish uchun 'oliver' deb yozing): "
+# ishora = True
+# while ishora:
+#     qiymat = input(savol)
+#     if qiymat == 'oliver':
+#         ishora = False
+#     else:
+#         print(int(qiymat)**2)
+
+# 7.
+# savol ="Kiritilgan sonning ildizini qaytaruvchi dastur.\n"
+# savol += "Musbat son kiriting "
+# savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+
+# while True:
+#     qiymat = input(savol)
+#     if qiymat<0:
+#         continue
+#     elif qiymat=='Exit':
+#         break
+#     else:
+#         ildiz = float(qiymat)**(0.5)
+#         print(f"{qiymat} ning ildizi {ildiz} ga teng") #bu holda shunaqa hatolik beradi'<' not supported between instances of 'str' and 'int'
+#
+# admin = ['milor' ,'179045']
+# a = input('isminizni kiriting:')
+# a += input('parolni kiriting:')
+# if admin == a:
+#     print("salom admin")
+# else:
+#     print("tizimga kiritaolmaymiz")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
